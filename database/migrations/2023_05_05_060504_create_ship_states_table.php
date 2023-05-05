4404c547d3734c48860d9688320f6d9eb703c8bc
Arafa Dev <arafa.dev@gmail.com>
2023-05-05 09:10:33 +0300

    Admin Panel Shipping Area Division Create Part 1(migrations)

---
+++ database/migrations/2023_05_05_060504_create_ship_states_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ship_states', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('division_id')->constrained('ship_divisions')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('district_id')->constrained('ship_districts')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('state_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ship_states');
+    }
+};
--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -134,12 +134,15 @@
             <a href="javascript:;" class="has-arrow">
                 <div class="parent-icon"><i class="bx bx-category"></i>
                 </div>
-                <div class="menu-title">Coupon Manage</div>
+                <div class="menu-title">Shipping Area </div>
             </a>
             <ul>
-                <li> <a href="{{ route('all.coupon') }}"><i class="bx bx-right-arrow-alt"></i>All Coupon</a>
+                <li> <a href="{{ route('all.coupon') }}"><i class="bx bx-right-arrow-alt"></i>All Division</a>
                 </li>
-                <li> <a href="{{ route('add.coupon') }}"><i class="bx bx-right-arrow-alt"></i>Add Coupon</a>
+                <li> <a href="{{ route('add.coupon') }}"><i class="bx bx-right-arrow-alt"></i>All District</a>
+                </li>
+
+                <li> <a href="{{ route('add.coupon') }}"><i class="bx bx-right-arrow-alt"></i>All State</a>
                 </li>
 
             </ul>
